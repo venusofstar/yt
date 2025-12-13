7c02f50100ca0c503292c89a818042bb659b1293
venusofstar <sumidomikeangelo24@gmail.com>
2025-12-14 03:37:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-import express from 'express'
-import { exec } from 'child_process'
+import express from "express"
+import { exec } from "child_process"
 
 const app = express()
 
-app.get('/m3u8/:id', (req, res) => {
+app.get("/m3u8/:id", (req, res) => {
   const id = req.params.id
 
   const cmd = `yt-dlp -f best --hls-use-mpegts -g https://www.youtube.com/watch?v=${id}`
 
-  exec(cmd, (err, stdout) => {
+  exec(cmd, { maxBuffer: 1024 * 1024 }, (err, stdout) => {
     if (err || !stdout) {
-      return res.status(500).send('yt-dlp failed')
+      return res.status(500).send("yt-dlp failed")
     }
 
     const streamUrl = stdout.trim()
 
-    res.setHeader('Content-Type', 'application/vnd.apple.mpegurl')
+    res.setHeader("Content-Type", "application/vnd.apple.mpegurl")
     res.send(`#EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-STREAM-INF:BANDWIDTH=8000000
@@ -23,4 +23,6 @@ ${streamUrl}`)
   })
 })
 
-app.listen(3000, () => console.log('yt-dlp backend ready'))
+app.listen(3000, () => {
+  console.log("yt-dlp backend running on port 3000")
+})
