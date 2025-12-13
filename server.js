2585783794154d0d1006cb4deb22664005225ffc
venusofstar <sumidomikeangelo24@gmail.com>
2025-12-14 03:31:09 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import { exec } from 'child_process'
+
+const app = express()
+
+app.get('/m3u8/:id', (req, res) => {
+  const id = req.params.id
+
+  const cmd = `yt-dlp -f best --hls-use-mpegts -g https://www.youtube.com/watch?v=${id}`
+
+  exec(cmd, (err, stdout) => {
+    if (err || !stdout) {
+      return res.status(500).send('yt-dlp failed')
+    }
+
+    const streamUrl = stdout.trim()
+
+    res.setHeader('Content-Type', 'application/vnd.apple.mpegurl')
+    res.send(`#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-STREAM-INF:BANDWIDTH=8000000
+${streamUrl}`)
+  })
+})
+
+app.listen(3000, () => console.log('yt-dlp backend ready'))
